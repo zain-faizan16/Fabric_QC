8c6e6ea7387173dc2cb5ee2d2ce16d0f948883ed
Muhammad Zain Arslan <zain.faizan16@gmail.com>
2020-09-07 19:56:39 +0500

    addded image results

--- Frame diffrence.py
+++ Frame diffrence.py
@@ -13,7 +13,7 @@ while(cap.isOpened()):
         gray2 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
         sub = gray1 - gray2
         frame = frame1
-        cv2.imwrite("frame%d.jpg" % currentFrame, sub)  
+        cv2.imwrite("data/frame%d.jpg" % currentFrame, sub)  
         currentFrame += 1  
         cv2.imshow('frame',sub)
         #cv2.waitKey(0)
